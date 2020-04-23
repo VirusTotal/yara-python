87141ba254dd4932012695a6d29d10a971537a80
Victor M. Alvarez <vmalvarez@virustotal.com>
2020-04-23 11:13:21 +0200

    Force metadata strings to be unicode.

--- tests.py
+++ tests.py
@@ -695,13 +695,13 @@ class TestYara(unittest.TestCase):
     def testMeta(self):
 
         r = yara.compile(source=r'rule test { meta: a = "foo\x80bar" condition: true }')
-        self.assertTrue(list(r)[0].meta['a'] == 'foobar')
+        self.assertTrue(list(r)[0].meta['a'] == u'foobar')
 
     # This test ensures that anything after the NULL character is stripped.
     def testMetaNull(self):
 
         r = yara.compile(source=r'rule test { meta: a = "foo\x00bar\x80" condition: true }')
-        self.assertTrue(list(r)[0].meta['a'] == 'foo')
+        self.assertTrue(list(r)[0].meta['a'] == u'foo')
 
     # This test is similar to testMeta but it tests the meta data generated
     # when a Match object is created.
@@ -709,7 +709,7 @@ class TestYara(unittest.TestCase):
 
         r = yara.compile(source=r'rule test { meta: a = "foo\x80bar" condition: true }')
         m = r.match(data='dummy')
-        self.assertTrue(list(m)[0].meta['a'] == 'foobar')
+        self.assertTrue(list(m)[0].meta['a'] == u'foobar')
 
     def testFilesize(self):
 
