050dd7e5a7e9ebad1657e6b84c9c2668d70e9262
Wesley Shields <wxs@atarininja.org>
2020-12-15 23:48:23 -0500

    Add support for exposing runtime warnings via a callback.
    Add support for a "warnings_callback" argument to Rules.match(). If provided the
    function definition needs to be:
    
    def warnings_callback(warning_type, message)
    
    The callback will be called with a warning type of yara.WARNING_TOO_MANY_MATCHES
    and the message will be a string indicating which rule caused the warning. I
    think a warning type and a message is reasonably flexible in case we introduce
    other runtime warnings in the future.
    
    If a callback is not provided we print a warning on stderr using the normal
    python warning system. It's worth noting the function I'm using was introduced
    in python 3.2. I can switch it to something more portable if you don't want to
    pull support for 2.x yet.
    
    While I'm here, also chase the renaming of rules_list_head and other list
    variables so that it can compile with latest yara master.
    
    I've also added a test case for the callback.

--- tests.py
+++ tests.py
@@ -267,6 +267,11 @@ RE_TESTS = [
 ]
 
 
+def warning_callback(warning_type, message):
+    global warning_callback_called
+    warning_callback_called = warning_type
+
+
 class TestYara(unittest.TestCase):
 
     def assertTrueRules(self, rules, data='dummy'):
@@ -1093,6 +1098,15 @@ class TestYara(unittest.TestCase):
 
         self.assertTrue(r.match(data=data))
 
+    def testWarningCallback(self):
+        global warning_callback_called
+
+        warning_callback_called = False
+        r = yara.compile(source='rule x { strings: $x = "X" condition: $x }')
+        data = memoryview(b"X" * 1000099)
+        r.match(data=data, warning_callback=warning_callback)
+
+        self.assertTrue(warning_callback_called, yara.WARNING_TOO_MANY_MATCHES)
 
 if __name__ == "__main__":
     unittest.main()
